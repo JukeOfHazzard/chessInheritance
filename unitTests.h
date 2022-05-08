fcf80094010bae1e4bb8ae52f88981a32a94b072
E Harrison <har16020@byui.edu>
2022-05-07 17:46:10 -0700

    finished pawn unit tests

--- unitTests.h
+++ unitTests.h
@@ -15,18 +15,253 @@ using namespace std;
 
 // todo: put runUnitTests() prototype here and definition in unitTests.cpp
 
+// todo: add the rest of the unit tests
+
+// todo: organize each of the unit tests into different files
+
 class TestPawn {
+private:
+    enum file {a, b, c, d, e, f, g, h}; // todo: potentially we can make these a part of Piece class
+    enum leftOrRight {left, right};
+
 public:
     void run() {
         // include all the test case methods here
-        test_case1();
+        test_white_enPassant_left();
+        test_white_enPassant_right();
+        test_black_enPassant_left();
+        test_black_enPassant_right();
+        
+        test_white_pawn_promotion();
+        test_black_pawn_promotion();
+        test_invalid_white_pawn_promotion();
+        test_invalid_black_pawn_promotion();
     }
     
-    void test_case1() {
-        //setup
-        //exercise
-        //verfify
-        //teardown
+    void test_white_pawn_promotion() {
+        // setup
+        Pawn p;
+        char board[64] = {
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            ' ', ' ', ' ', 'p', ' ', ' ', ' ', ' ',
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+        int col = d; // start file
+        int row = 7; // start rank
+        p.location = (row-1) * 8 + col;
+        
+        // exercise
+        p.move_normal(); // moves from D7 to D8
+        
+        // verify
+        assert(p.doesPromote());
+        
+        // teardown: N/A
+    }
+    
+    void test_black_pawn_promotion() {
+        // setup
+        Pawn p;
+        char board[64] = {
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            ' ', ' ', ' ', 'P', ' ', ' ', ' ', ' ',
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+        int col = d; // start file
+        int row = 2; // start rank
+        p.location = (row-1) * 8 + col;
+
+        // exercise
+        p.move_normal(); // moves from D2 to D1
+        
+        // verify
+        assert(p.doesPromote());
+        
+        // teardown: N/A
+    }
+    
+    void test_invalid_white_pawn_promotion() {
+        // setup
+        Pawn p;
+        char board[64] = {
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            ' ', ' ', ' ', 'p', ' ', ' ', ' ', ' ',
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+        int col = d; // start file
+        int row = 6; // start rank
+        p.location = (row-1) * 8 + col;
+        
+        // exercise
+        p.move_normal(); // moves from D6 to D7
+        
+        // verify
+        assert(!p.doesPromote());
+        
+        // teardown: N/A
+    }
+    
+    void test_invalid_black_pawn_promotion() {
+        // setup
+        Pawn p;
+        char board[64] = {
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            ' ', ' ', ' ', 'P', ' ', ' ', ' ', ' ',
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+        int col = d; // start file
+        int row = 3; // start rank
+        p.location = (row-1) * 8 + col;
+
+        // exercise
+        p.move_normal(); // moves from D2 to D1
+        
+        // verify
+        assert(!p.doesPromote());
+        
+        // teardown: N/A
+    }
+    
+    void test_white_enPassant_left() {
+        // setup
+        Pawn p;
+        char board[64] = {
+            'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r',
+            'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p',
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            ' ', ' ', 'P', 'p', ' ', ' ', ' ', ' ', // 'p' position is D5 (in this instance)
+            // *white moved two, black moved two, white moved one, black to LEFT of white
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P',
+            'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'};
+        // white must be in 5th rank and go to 6th rank
+        int row = 5; // start rank
+        int col = d; // start file
+        p.location = (row-1) * 8 + col;
+        
+        // exercise
+        // white moves diagonally to the left
+        p.move_enPassant(left);
+        // still messing around with methods/parameters at this point,
+        // I figured this is the simplest in terms of readability
+        
+        // verfify
+        int newRow = row + 1; // end rank
+        int newCol = col - 1; // end file
+        int endLocation = newRow * 8 + newCol;
+        assert(p.location == endLocation);
+        
+        // teardown == N/A
+    }
+    
+    void test_white_enPassant_right() {
+        // setup
+        Pawn p;
+        char board[64] = {
+            'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r',
+            'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p',
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            ' ', ' ', ' ', 'p', 'P', ' ', ' ', ' ',
+            // *white moved two, black moved two, white moved one, black to RIGHT of white
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P',
+            'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R' };
+        // white must be in 5th rank and go to 6th rank
+        int row = 5; // start rank
+        int col = d; // start file
+        p.location = (row-1) * 8 + col;
+        
+        // exercise
+        // white moves diagonally to the left
+        p.move_enPassant(right);
+        
+        // verfify
+        int newRow = row + 1; // end rank
+        int newCol = col + 1; // end file
+        int endLocation = newRow * 8 + newCol;
+        assert(p.location == endLocation);
+        
+        // teardown == N/A
+    }
+    
+    void test_black_enPassant_left() {
+        // setup
+        Pawn p;
+        char board[64] = {
+            'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r',
+            'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p',
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            ' ', 'p', 'P', ' ', ' ', ' ', ' ', ' ', // 'P' position is C4 (in this instance)
+            // *black moved two, white moved two, black moved one, white to LEFT of white
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P',
+            'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R' };
+        // white must be in 5th rank and go to 6th rank
+        int row = 4; // start rank
+        int col = c; // start file
+        p.location = (row-1) * 8 + col;
+        
+        // exercise
+        // white moves diagonally to the left
+        p.move_enPassant(left);
+        
+        // verfify
+        int newRow = row - 1; // end rank
+        int newCol = col - 1; // end file
+        int endLocation = newRow * 8 + newCol;
+        assert(p.location == endLocation);
+        
+        // teardown == N/A
+    }
+    
+    void test_black_enPassant_right() {
+        // setup
+        Pawn p;
+        char board[64] = {
+            'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r',
+            'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p',
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            ' ', ' ', 'P', 'p', ' ', ' ', ' ', ' ', // 'P' position is C4 (in this instance)
+            // *black moved two, white moved two, black moved one, white to RIGHT of white
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P',
+            'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R' };
+        // white must be in 5th rank and go to 6th rank
+        int row = 4; // start rank
+        int col = c; // start file
+        p.location = (row-1) * 8 + col;
+        
+        // exercise
+        // white moves diagonally to the left
+        p.move_enPassant(right);
+        
+        // verfify
+        int newRow = row - 1; // end rank
+        int newCol = col + 1; // end file
+        int endLocation = newRow * 8 + newCol;
+        assert(p.location == endLocation);
+        
+        // teardown == N/A
     }
 };
 
