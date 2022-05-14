46851fc48585327d05efcdbd12de94657b7e333c
E Harrison <har16020@byui.edu>
2022-05-14 12:17:42 -0700

    added Position and Move classes

---
+++ Position.cpp
@@ -0,0 +1,8 @@
+//
+//  Position.cpp
+//  Lab01
+//
+//  Created by Elijah Harrison on 5/14/22.
+//
+
+#include "Position.h"
---
+++ Position.h
@@ -0,0 +1,15 @@
+//
+//  Position.h
+//  Lab01
+//
+//  Created by Elijah Harrison on 5/14/22.
+//
+
+#ifndef Position_h
+#define Position_h
+
+class Position {
+    
+};
+
+#endif /* Position_h */
